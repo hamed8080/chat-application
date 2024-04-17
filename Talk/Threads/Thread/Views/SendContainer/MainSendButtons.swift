6c43f4eda80bc5fac3b89120cc15ecf7a64f45de
Hamed Hosseini <hamed8080@gmail.com>
2024-04-17 16:54:41 +0330

    - Refactor the main send buttons UI - Fix animation of switching between buttons - Hold to record audio

--- Talk/Threads/Thread/Views/SendContainer/MainSendButtons.swift
+++ Talk/Threads/Thread/Views/SendContainer/MainSendButtons.swift
@@ -20,42 +20,49 @@ struct MainSendButtons: View {
 
     var body: some View {
         HStack(alignment: .bottom, spacing: 8) {
-            Button {
-                withAnimation(animation(appear: !viewModel.showActionButtons)) {
-                    viewModel.showActionButtons.toggle()
-                }
-            } label: {
-                Image(systemName: viewModel.showActionButtons ? "chevron.down" : "paperclip")
-                    .interpolation(.none)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: viewModel.showActionButtons ? 16 : 20, height: viewModel.showActionButtons ? 16 : 20)
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(Color.App.accent)
-                    .fontWeight(.medium)
+            actionButtonsToggleView
+            textFieldView
+            micOrCameraView
+            sendButtonView
+        }
+        .animation(.easeInOut, value: viewModel.isVideoRecordingSelected)
+        .fullScreenCover(isPresented: $showCaptureImageView) {
+            CameraCapturer(isVideo: false) { image, _, asset in
+                onImageCapture(image)
             }
-            .frame(width: 42, height: 42)
-            .background(Color.App.bgSendInput)
-            .clipShape(RoundedRectangle(cornerRadius:(22)))
-            .buttonStyle(.borderless)
-            .fontWeight(.light)
+            .ignoresSafeArea()
+        }
+        .fullScreenCover(isPresented: $showCaptureVideoView) {
+            CameraCapturer(isVideo: true) { _, videoURL, asset in
+                onVideoCapture(videoURL)
+            }
+            .ignoresSafeArea()
+        }
+    }
 
-            MultilineTextField(
-                "Thread.SendContainer.typeMessageHere",
-                text: $viewModel.textMessage,
-                textColor: UIColor(named: "text_primary"),
-                backgroundColor: Color.App.bgSendInput,
-                placeholderColor: Color.App.textPrimary.opacity(0.7),
-                mention: true,
-                focus: $viewModel.focusOnTextInput
-            )
-            .clipShape(RoundedRectangle(cornerRadius:(24)))
-            .environment(\.layoutDirection, Locale.current.identifier.contains("fa") ? .rightToLeft : .leftToRight)
+    private func animation(appear: Bool) -> Animation {
+        appear ? .spring(response: 0.4, dampingFraction: 0.5, blendDuration: 0.2) : .easeOut(duration: 0.13)
+    }
 
-            Button {
-                threadVM.attachmentsViewModel.clear()
-                threadVM.setupRecording()
-            } label: {
+    private func onImageCapture(_ image: UIImage?) {
+        guard let image = image else { return }
+        let item = ImageItem(data: image.jpegData(compressionQuality: 80) ?? Data(),
+                             width: Int(image.size.width),
+                             height: Int(image.size.height),
+                             originalFilename: "Talk-\(Date().millisecondsSince1970).jpg")
+        threadVM.attachmentsViewModel.addSelectedPhotos(imageItem: item)
+        threadVM.animateObjectWillChange()
+    }
+
+    private func onVideoCapture(_ videoURL: URL?) {
+        guard let videoURL = videoURL else { return }
+        threadVM.attachmentsViewModel.addFileURL(url: videoURL)
+        threadVM.animateObjectWillChange()
+    }
+
+    private var micOrCameraView: some View {
+        ZStack {
+            HStack {
                 Image(systemName: "mic")
                     .interpolation(.none)
                     .resizable()
@@ -65,16 +72,18 @@ struct MainSendButtons: View {
                     .foregroundStyle(Color.App.textSecondary)
             }
             .frame(width: viewModel.showAudio ? 48 : 0, height: viewModel.showAudio ? 48 : 0)
-            .buttonStyle(.borderless)
             .fontWeight(.light)
-            //            .keyboardShortcut(.init("r"), modifiers: [.command]) // if enabled we may have memory leak when press the back button in ThreadView check if it works properly.
-            .highPriorityGesture(switchRecordingGesture)
-            .transition(.asymmetric(insertion: .move(edge: .bottom).animation(.easeIn(duration: 0.2)), removal: .push(from: .top).animation(.easeOut(duration: 0.2))))
+            .onTapGesture {
+                viewModel.isVideoRecordingSelected.toggle()
+            }
+            .onLongPressGesture {
+                threadVM.attachmentsViewModel.clear()
+                threadVM.setupRecording()
+            }
+            .transition(.opacity)
 
             if viewModel.showCamera {
-                Button {
-                    showPopover.toggle()
-                } label: {
+                HStack {
                     Image(systemName: "camera")
                         .interpolation(.none)
                         .resizable()
@@ -84,7 +93,6 @@ struct MainSendButtons: View {
                         .foregroundStyle(Color.App.textSecondary)
                 }
                 .frame(width: 48, height: 48)
-                .buttonStyle(.borderless)
                 .fontWeight(.light)
                 .popover(isPresented: $showPopover) {
                     VStack(alignment: .leading, spacing: 32) {
@@ -109,68 +117,76 @@ struct MainSendButtons: View {
                     .padding()
                     .presentationCompactAdaptation(.popover)
                 }
-                //                .keyboardShortcut(.init("r"), modifiers: [.command]) // if enabled we may have memory leak when press the back button in ThreadView check if it works properly.
-                .highPriorityGesture(switchRecordingGesture)
-                .transition(.asymmetric(insertion: .move(edge: .bottom).animation(.easeIn(duration: 0.2)), removal: .push(from: .top).animation(.easeOut(duration: 0.2))))
-            }
-
-            if viewModel.showSendButton {
-                Button {
-                    if viewModel.showSendButton {
-                        threadVM.sendMessageViewModel.sendTextMessage()
-                    }
-                    threadVM.mentionListPickerViewModel.text = ""
-                    threadVM.sheetType = nil
-                    threadVM.animateObjectWillChange()
-                } label: {
-                    Image(systemName: "arrow.up.circle.fill")
-                        .interpolation(.none)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: viewModel.showSendButton ? 26 : 0, height: viewModel.showSendButton ? 26 : 0)
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(Color.App.white, Color.App.accent)
-                }
-                .frame(width: viewModel.showSendButton ? 48 : 0, height: viewModel.showSendButton ? 48 : 0)
-                .buttonStyle(.borderless)
-                .fontWeight(.light)
-                //            .keyboardShortcut(.return, modifiers: [.command]) // if enabled we may have memory leak when press the back button in ThreadView check if it works properly.
-            }
-        }
-        .animation(.spring(response: 0.4, dampingFraction: 0.5, blendDuration: 0.3), value: viewModel.isVideoRecordingSelected)
-        .fullScreenCover(isPresented: $showCaptureImageView) {
-            CameraCapturer(isVideo: false) { image, _, asset in
-                guard let image = image else { return }
-                let item = ImageItem(data: image.jpegData(compressionQuality: 80) ?? Data(),
-                                     width: Int(image.size.width),
-                                     height: Int(image.size.height),
-                                     originalFilename: "Talk-\(Date().millisecondsSince1970).jpg")
-                threadVM.attachmentsViewModel.addSelectedPhotos(imageItem: item)
-                threadVM.animateObjectWillChange()
-            }
-            .ignoresSafeArea()
-        }
-        .fullScreenCover(isPresented: $showCaptureVideoView) {
-            CameraCapturer(isVideo: true) { _, videoURL, asset in
-                guard let videoURL = videoURL else { return }
-                threadVM.attachmentsViewModel.addFileURL(url: videoURL)
-                threadVM.animateObjectWillChange()
-            }
-            .ignoresSafeArea()
-        }
-    }
-
-    private func animation(appear: Bool) -> Animation {
-        appear ? .spring(response: 0.4, dampingFraction: 0.5, blendDuration: 0.2) : .easeOut(duration: 0.13)
-    }
-
-    private var switchRecordingGesture: some Gesture {
-        DragGesture(minimumDistance: 10, coordinateSpace: .local)
-            .onEnded { newValue in
-                if abs(newValue.translation.height) > 32 {
+                .onTapGesture {
                     viewModel.isVideoRecordingSelected.toggle()
                 }
+                .onLongPressGesture {
+                    showPopover.toggle()
+                }
+                .transition(.opacity)
             }
+        }
+    }
+
+    @ViewBuilder
+    private var sendButtonView: some View {
+        if viewModel.showSendButton {
+            Button {
+                if viewModel.showSendButton {
+                    threadVM.sendMessageViewModel.sendTextMessage()
+                }
+                threadVM.mentionListPickerViewModel.text = ""
+                threadVM.sheetType = nil
+                threadVM.animateObjectWillChange()
+            } label: {
+                Image(systemName: "arrow.up.circle.fill")
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: viewModel.showSendButton ? 26 : 0, height: viewModel.showSendButton ? 26 : 0)
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(Color.App.white, Color.App.accent)
+            }
+            .frame(width: viewModel.showSendButton ? 48 : 0, height: viewModel.showSendButton ? 48 : 0)
+            .buttonStyle(.borderless)
+            .fontWeight(.light)
+        }
+    }
+
+    private var actionButtonsToggleView: some View {
+        Button {
+            withAnimation(animation(appear: !viewModel.showActionButtons)) {
+                viewModel.showActionButtons.toggle()
+            }
+        } label: {
+            Image(systemName: viewModel.showActionButtons ? "chevron.down" : "paperclip")
+                .interpolation(.none)
+                .resizable()
+                .scaledToFit()
+                .frame(width: viewModel.showActionButtons ? 16 : 20, height: viewModel.showActionButtons ? 16 : 20)
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(Color.App.accent)
+                .fontWeight(.medium)
+        }
+        .frame(width: 42, height: 42)
+        .background(Color.App.bgSendInput)
+        .clipShape(RoundedRectangle(cornerRadius:(22)))
+        .buttonStyle(.borderless)
+        .fontWeight(.light)
+    }
+
+    private var textFieldView: some View {
+        MultilineTextField(
+            "Thread.SendContainer.typeMessageHere",
+            text: $viewModel.textMessage,
+            textColor: UIColor(named: "text_primary"),
+            backgroundColor: Color.App.bgSendInput,
+            placeholderColor: Color.App.textPrimary.opacity(0.7),
+            mention: true,
+            focus: $viewModel.focusOnTextInput
+        )
+        .clipShape(RoundedRectangle(cornerRadius:(24)))
+        .environment(\.layoutDirection, Locale.current.identifier.contains("fa") ? .rightToLeft : .leftToRight)
     }
 }
 
